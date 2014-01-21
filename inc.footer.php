e473a94f2ebb6384c23841852726425c03165825
Shehan Tis <shehanproductions@ymail.com>
2014-01-21 23:48:24 +0530

    added new subpages

---
+++ inc.footer.php
@@ -0,0 +1,74 @@
+<?php ?>
+<footer id="footer" class="site-footer">
+        <div class="container">
+
+          <div id="sidebar-footer-1">
+            <div class="row">
+
+              <aside class="widget col-md-3 col-sm-6">
+                <h5 class="widget-title">About Us</h5>
+                <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptate rerum voluptates. <a href="#">Read more</a></p>
+              </aside>
+
+              <aside class="widget col-md-3 col-sm-6">
+                <h5 class="widget-title">Contact</h5>
+                <div class="textwidget">
+                  <div class="adr">
+                    <div class="street-address">416 Water St.</div>
+                    <span class="locality">New York</span>,
+                    <abbr class="region" title="New York">NY</abbr>,
+                    <span class="postal-code">10002</span>,
+                    <span class="country-name">U.S.A</span>
+                  </div>
+                  <div>Phone: <span class="tel">+1.123.456.7890</span></div>
+                  <div>Website: <a class="url" href="http://www.yoarts.com/">http://www.yoarts.com</a></div>
+                </div>
+              </aside>
+
+              <aside class="widget col-md-3 col-sm-6">
+                <h5 class="widget-title">Need Help?</h5>
+                <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptate rerum voluptates. <a href="#">Read more</a></p>
+              </aside>
+
+              <aside class="widget col-md-3 col-sm-6">
+                <h5 class="widget-title">Get in touch</h5>
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Your Email">
+                  <span class="input-group-btn">
+                    <button class="btn btn-danger" type="button"><i class="fa fa-arrow-right"></i></button>
+                  </span>
+                </div>
+                <ul class="social-links">
+                  <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                  <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                </ul>
+              </aside>
+
+            </div>
+          </div>
+
+          <div id="sidebar-footer-2">
+            <div class="row">
+
+              <aside class="widget col-sm-6">
+                <h5 class="widget-title">Latest Tweet</h5>
+                <div class="tweet">
+                  It is better to be hated for what you are than to be loved for something you are not.<br />– Andre Gide –
+                </div>
+              </aside>
+
+              <aside class="widget widget_flickr col-sm-6">
+                <h5 class="widget-title">Latest Photos</h5>
+                <a href="#"><img src="images/photo-2.jpg" /></a>
+                <a href="#"><img src="images/photo-3.jpg" /></a>
+                <a href="#"><img src="images/photo-1.jpg" /></a>
+              </aside>
+
+            </div>
+          </div>
+
+          <div class="copyright"><p>© 2014 Company, Inc.<br /> Theme: Start 1.0.5. Designed by <a href="http://www.yoarts.com/">YoArts</a></p></div>
+        </div>
+      </footer>
---
+++ inc.header.php
@@ -0,0 +1,18 @@
+<?php ?>
+<header id="header" class="site-header">
+        <div class="container">
+          <h1 class="navbar-brand"><a href="index.php">Roy Tours</a></h1>
+          <ul id="navigation" class="nav nav-pills pull-right">
+            <li class="active"><a href="#carousel-1" class="current">Home</a></li>
+            <li><a href="#features">About Me</a></li>
+			<li><a href="#portfolio">Sri Lanka</a></li>
+			<li><a href="#testimonials">Maldives</a></li>
+			<li><a href="#blog">Hotels</a></li>
+            <li><a href="#clients">Tours</a></li>
+            <!--<li><a href="#testimonials">Testimonials</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#clients">Clients</a></li>-->
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </header>
