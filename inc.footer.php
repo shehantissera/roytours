232e2918638ae0e184d703a4a3069dca5c56664b
Shehan Tis <shehanproductions@ymail.com>
2014-01-22 00:35:12 +0530

    edited the footer and some other small changers has been done

--- inc.footer.php
+++ inc.footer.php
@@ -7,13 +7,14 @@
 
               <aside class="widget col-md-3 col-sm-6">
                 <h5 class="widget-title">About Us</h5>
-                <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptate rerum voluptates. <a href="#">Read more</a></p>
+                <p>Here goes a little information taken from the about us page and when this is clicked it will be taken where about us information is available. <a href="#features">Read more</a></p>
               </aside>
 
               <aside class="widget col-md-3 col-sm-6">
                 <h5 class="widget-title">Contact</h5>
                 <div class="textwidget">
                   <div class="adr">
+					<div class="street-address">Roy Tissera</div>
                     <div class="street-address">416 Water St.</div>
                     <span class="locality">New York</span>,
                     <abbr class="region" title="New York">NY</abbr>,
@@ -21,7 +22,7 @@
                     <span class="country-name">U.S.A</span>
                   </div>
                   <div>Phone: <span class="tel">+1.123.456.7890</span></div>
-                  <div>Website: <a class="url" href="http://www.yoarts.com/">http://www.yoarts.com</a></div>
+                  <div>Website: <a class="url" href="http://www.yoarts.com/">www.RoyTours.com</a></div>
                 </div>
               </aside>
 
@@ -49,26 +50,6 @@
             </div>
           </div>
 
-          <div id="sidebar-footer-2">
-            <div class="row">
-
-              <aside class="widget col-sm-6">
-                <h5 class="widget-title">Latest Tweet</h5>
-                <div class="tweet">
-                  It is better to be hated for what you are than to be loved for something you are not.<br />– Andre Gide –
-                </div>
-              </aside>
-
-              <aside class="widget widget_flickr col-sm-6">
-                <h5 class="widget-title">Latest Photos</h5>
-                <a href="#"><img src="images/photo-2.jpg" /></a>
-                <a href="#"><img src="images/photo-3.jpg" /></a>
-                <a href="#"><img src="images/photo-1.jpg" /></a>
-              </aside>
-
-            </div>
-          </div>
-
-          <div class="copyright"><p>© 2014 Company, Inc.<br /> Theme: Start 1.0.5. Designed by <a href="http://www.yoarts.com/">YoArts</a></p></div>
+          <div class="copyright"><p>© 2014 RoyTours.com<br /> Developed by <a href="#">Tistus</a></p></div>
         </div>
       </footer>
